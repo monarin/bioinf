afaf7cb7be738b15d7f1f9ca18d21f57adef53c1
Stephen Kelly <stevekm@users.noreply.github.com>
2023-06-28 21:35:28 -0400

    apply black formatting

--- translator.py
+++ translator.py
@@ -1,118 +1,147 @@
 """ translates transcript coordinates to genomic coordinates
 or vise versa"""
 
-#Define exceptions
-class TranslatorError(Exception): pass
-class InvalidTranscriptName(TranslatorError): pass
-class InvalidTranscriptCoordinate(TranslatorError): pass
-class InvalidChromosomeName(TranslatorError): pass
-class InvalidChromosomeCoordinate(TranslatorError): pass
+# Define exceptions
+class TranslatorError(Exception):
+    pass
+
+
+class InvalidTranscriptName(TranslatorError):
+    pass
+
+
+class InvalidTranscriptCoordinate(TranslatorError):
+    pass
+
+
+class InvalidChromosomeName(TranslatorError):
+    pass
+
+
+class InvalidChromosomeCoordinate(TranslatorError):
+    pass
+
 
 import re, argparse
 import readCIGAR
 import readQuery
 
-def createTranslateTbl(CIGARObj, mode='TR2CHR'):
-	"""create a table of matching starting positions for
-	transcript and genomic coordinates"""
-	translateTbl = {}
-	for TRName, TRVal in CIGARObj.items():
-		for CHRName, CHRVal in TRVal.items():
-			startingPosition, CIGARStr = CHRVal
-			#convert to starting value of each section
-			CIGARMatch = re.findall(r'(\d+)(\w)', CIGARStr)
-			try:
-				CIGARValTR = [int(pair[0]) * int(pair[1] in ('M','I')) for pair in CIGARMatch] + [0]
-				CIGARValCHR = [int(pair[0]) * int(pair[1] in ('M','D')) for pair in CIGARMatch] + [0]
-				if mode == 'TR2CHR':
-					CIGARList = [(sum(CIGARValTR[:i]), sum(CIGARValCHR[:i])+startingPosition) \
-							for i in range(len(CIGARMatch)+1)]
-				else:
-					CIGARList = [(sum(CIGARValCHR[:i])+startingPosition, sum(CIGARValTR[:i])) \
-							for i in range(len(CIGARMatch)+1)]
-			except ValueError:
-				print("Error calculating CIGAR value, exiting")
-				exit()
-			if mode == 'TR2CHR':
-				if TRName in translateTbl:
-					translateTbl[TRName][CHRName] = CIGARList
-				else:
-					translateTbl[TRName] = {CHRName: CIGARList}
-			else:
-				#assume mode = 'CHR2TR'
-				if CHRName in translateTbl:
-					translateTbl[CHRName][TRName] = CIGARList
-				else:
-					translateTbl[CHRName] = {TRName: CIGARList}
-	return translateTbl
+
+def createTranslateTbl(CIGARObj, mode="TR2CHR"):
+    """create a table of matching starting positions for
+    transcript and genomic coordinates"""
+    translateTbl = {}
+    for TRName, TRVal in CIGARObj.items():
+        for CHRName, CHRVal in TRVal.items():
+            startingPosition, CIGARStr = CHRVal
+            # convert to starting value of each section
+            CIGARMatch = re.findall(r"(\d+)(\w)", CIGARStr)
+            try:
+                CIGARValTR = [
+                    int(pair[0]) * int(pair[1] in ("M", "I")) for pair in CIGARMatch
+                ] + [0]
+                CIGARValCHR = [
+                    int(pair[0]) * int(pair[1] in ("M", "D")) for pair in CIGARMatch
+                ] + [0]
+                if mode == "TR2CHR":
+                    CIGARList = [
+                        (sum(CIGARValTR[:i]), sum(CIGARValCHR[:i]) + startingPosition)
+                        for i in range(len(CIGARMatch) + 1)
+                    ]
+                else:
+                    CIGARList = [
+                        (sum(CIGARValCHR[:i]) + startingPosition, sum(CIGARValTR[:i]))
+                        for i in range(len(CIGARMatch) + 1)
+                    ]
+            except ValueError:
+                print("Error calculating CIGAR value, exiting")
+                exit()
+            if mode == "TR2CHR":
+                if TRName in translateTbl:
+                    translateTbl[TRName][CHRName] = CIGARList
+                else:
+                    translateTbl[TRName] = {CHRName: CIGARList}
+            else:
+                # assume mode = 'CHR2TR'
+                if CHRName in translateTbl:
+                    translateTbl[CHRName][TRName] = CIGARList
+                else:
+                    translateTbl[CHRName] = {TRName: CIGARList}
+    return translateTbl
+
 
 def toCHR(translateTbl, TRName, TR):
-	"""Input: translate table, transcript name, and transcript coordinate
-	Use translateTbl to convert from transcript coordinate to
-	genomic coordinate"""
-	if TR < 0:
-		raise InvalidTranscriptCoordinate("Transcript coodinate is invalid")
-	output = []
-	txt = ''
-	if TRName in translateTbl:
-		for CHRName, translateTR in translateTbl[TRName].items():
-			for i in range(len(translateTR)):
-				TRCoord, CHRCoord = translateTR[i]
-				prevCHRCoord = 0 if i == 0 else translateTR[i-1][1]
-				if TRCoord > TR: break
-			CHRMap = CHRCoord-(TRCoord-TR)
-			if CHRMap > CHRCoord:
-				raise InvalidTranscriptCoordinate("Transcript coodinate does not exist")
-			elif CHRMap < prevCHRCoord:
-				CHRMap = 'I'+str(CHRCoord)
-			output += [(TRName, TR, CHRName, CHRMap)]
-			txt += TRName+'\t'+str(TR)+'\t'+CHRName+'\t'+str(CHRMap)+'\n'
-	else:
-		raise InvalidTranscriptName("Transcript name not found in input 1")
-	return output, txt
+    """Input: translate table, transcript name, and transcript coordinate
+    Use translateTbl to convert from transcript coordinate to
+    genomic coordinate"""
+    if TR < 0:
+        raise InvalidTranscriptCoordinate("Transcript coodinate is invalid")
+    output = []
+    txt = ""
+    if TRName in translateTbl:
+        for CHRName, translateTR in translateTbl[TRName].items():
+            for i in range(len(translateTR)):
+                TRCoord, CHRCoord = translateTR[i]
+                prevCHRCoord = 0 if i == 0 else translateTR[i - 1][1]
+                if TRCoord > TR:
+                    break
+            CHRMap = CHRCoord - (TRCoord - TR)
+            if CHRMap > CHRCoord:
+                raise InvalidTranscriptCoordinate("Transcript coodinate does not exist")
+            elif CHRMap < prevCHRCoord:
+                CHRMap = "I" + str(CHRCoord)
+            output += [(TRName, TR, CHRName, CHRMap)]
+            txt += TRName + "\t" + str(TR) + "\t" + CHRName + "\t" + str(CHRMap) + "\n"
+    else:
+        raise InvalidTranscriptName("Transcript name not found in input 1")
+    return output, txt
+
 
 def toTR(translateTbl, CHRName, CHR):
-	"""Input: translate table, chromosome name, and chromosome coordinate
-	Use translateTbl to convert from chromosome coordinate to
-	transcript coordinate"""
-	if CHR < 0:
-		raise InvalidChromosomeCoordinate("Chromosome coodinate is invalid")
-	output = []
-	txt = ''
-	if CHRName in translateTbl:
-		for TRName, translateTR in translateTbl[CHRName].items():
-			for i in range(len(translateTR)):
-				CHRCoord, TRCoord = translateTR[i]
-				prevTRCoord = 0 if i == 0 else translateTR[i-1][1]
-				if CHRCoord > CHR: break
-			TRMap = TRCoord-(CHRCoord-CHR)
-			if TRMap > TRCoord:
-				raise InvalidChromosomeCoordinate("Chromosome coodinate does not exist")
-			elif TRMap < prevTRCoord:
-				TRMap = 'D'+str(TRCoord)
-			output += [(CHRName, CHR, TRName, TRMap)]
-			txt += CHRName+'\t'+str(CHR)+'\t'+TRName+'\t'+str(TRMap)+'\n'
-	else:
-		raise InvalidChromosomeName("Chromosome name not found in input 1")
-	return output, txt
+    """Input: translate table, chromosome name, and chromosome coordinate
+    Use translateTbl to convert from chromosome coordinate to
+    transcript coordinate"""
+    if CHR < 0:
+        raise InvalidChromosomeCoordinate("Chromosome coodinate is invalid")
+    output = []
+    txt = ""
+    if CHRName in translateTbl:
+        for TRName, translateTR in translateTbl[CHRName].items():
+            for i in range(len(translateTR)):
+                CHRCoord, TRCoord = translateTR[i]
+                prevTRCoord = 0 if i == 0 else translateTR[i - 1][1]
+                if CHRCoord > CHR:
+                    break
+            TRMap = TRCoord - (CHRCoord - CHR)
+            if TRMap > TRCoord:
+                raise InvalidChromosomeCoordinate("Chromosome coodinate does not exist")
+            elif TRMap < prevTRCoord:
+                TRMap = "D" + str(TRCoord)
+            output += [(CHRName, CHR, TRName, TRMap)]
+            txt += CHRName + "\t" + str(CHR) + "\t" + TRName + "\t" + str(TRMap) + "\n"
+    else:
+        raise InvalidChromosomeName("Chromosome name not found in input 1")
+    return output, txt
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(
-			description="Translator between transcript and genomic coordinates")
-	parser.add_argument(
-			'input1',
-			metavar='INPUT1',
-			help = 'A four column (tab-separated) file containing the transcripts')
-	parser.add_argument(
-			'input2',
-			metavar='INPUT2',
-			help = 'A two column (tab-separated) file indicating a set of queries')
-	args = parser.parse_args()
-	CIGARObj = readCIGAR.read(args.input1)
-	query = readQuery.read(args.input2)
-	translateTbl = createTranslateTbl(CIGARObj)
-	txt = ''.join([toCHR(translateTbl, TRPair[0], TRPair[1])[1] for TRPair in query])
-	print('Transcript to genmoic coordinate translator\nOUTPUT:')
-	print(txt)
-
-
+    parser = argparse.ArgumentParser(
+        description="Translator between transcript and genomic coordinates"
+    )
+    parser.add_argument(
+        "input1",
+        metavar="INPUT1",
+        help="A four column (tab-separated) file containing the transcripts",
+    )
+    parser.add_argument(
+        "input2",
+        metavar="INPUT2",
+        help="A two column (tab-separated) file indicating a set of queries",
+    )
+    args = parser.parse_args()
+    CIGARObj = readCIGAR.read(args.input1)
+    query = readQuery.read(args.input2)
+    translateTbl = createTranslateTbl(CIGARObj)
+    txt = "".join([toCHR(translateTbl, TRPair[0], TRPair[1])[1] for TRPair in query])
+    print("Transcript to genmoic coordinate translator\nOUTPUT:")
+    print(txt)
--- translatortest.py
+++ translatortest.py
@@ -2,48 +2,79 @@
 import translator
 import unittest
 
-class KnownValues(unittest.TestCase):
-	CIGARObj = {'TR1': {'CHR1': (3, '8M7D6M2I2M11D7M')},
-			'TR2': {'CHR2': (10, '20M')}}
-	knownTranslateTbl_TR2CHR = {'TR1': {'CHR1': [(0, 3), (8, 11), (8, 18), (14, 24), (16, 24), (18, 26), (18, 37), (25, 44)]}, \
-			'TR2': {'CHR2': [(0, 10), (20, 30)]}}
-	knownTranslateTbl_CHR2TR = {'CHR1': {'TR1': [(3, 0), (11, 8), (18, 8), (24, 14), (24, 16), (26, 18), (37, 18), (44, 25)]}, \
-			'CHR2': {'TR2': [(10, 0), (30, 20)]}}
-	knownValuesToCHR = (('TR1', 4, 'CHR1', 7),
-			('TR1', 14, 'CHR1', 'I24'), 
-			('TR1', 0, 'CHR1', 3),
-			('TR1', 24, 'CHR1', 43),
-			('TR2', 0, 'CHR2', 10),
-			('TR1', 13, 'CHR1', 23),
-			('TR2', 10, 'CHR2', 20))
-	knownValuesToTR = (('CHR1', 7, 'TR1', 4),
-			('CHR1', 12, 'TR1', 'D8'),
-			('CHR2', 10, 'TR2', 0),
-			('CHR1', 23, 'TR1', 13),
-			('CHR2', 20, 'TR2', 10))
-	
-	def testCreateTranslateTbl(self):
-		"""createTranslateTbl should give known result"""
-		result_TR2CHR = translator.createTranslateTbl(self.CIGARObj)
-		self.assertEqual(self.knownTranslateTbl_TR2CHR, result_TR2CHR)
-		result_CHR2TR = translator.createTranslateTbl(self.CIGARObj, mode='CHR2TR')
-		self.assertEqual(self.knownTranslateTbl_CHR2TR, result_CHR2TR)
 
-	def testToCHRKnownValues(self):
-		"""toCHR should give known result with known input"""
-		translateTbl = translator.createTranslateTbl(self.CIGARObj)
-		for item in self.knownValuesToCHR:
-			TRName, TR, CHRName, CHR = item
-			result, _ = translator.toCHR(translateTbl, TRName, TR)
-			self.assertEqual([item], result)
-	
-	def testToTRKnownValue(self):
-		"""toTR should give known result with known input"""
-		translateTbl = translator.createTranslateTbl(self.CIGARObj, mode='CHR2TR')
-		for item in self.knownValuesToTR:
-			CHRName, CHR, TRName, TR = item
-			result, _ = translator.toTR(translateTbl, CHRName, CHR)
-			self.assertEqual([item], result)
+class KnownValues(unittest.TestCase):
+    CIGARObj = {"TR1": {"CHR1": (3, "8M7D6M2I2M11D7M")}, "TR2": {"CHR2": (10, "20M")}}
+    knownTranslateTbl_TR2CHR = {
+        "TR1": {
+            "CHR1": [
+                (0, 3),
+                (8, 11),
+                (8, 18),
+                (14, 24),
+                (16, 24),
+                (18, 26),
+                (18, 37),
+                (25, 44),
+            ]
+        },
+        "TR2": {"CHR2": [(0, 10), (20, 30)]},
+    }
+    knownTranslateTbl_CHR2TR = {
+        "CHR1": {
+            "TR1": [
+                (3, 0),
+                (11, 8),
+                (18, 8),
+                (24, 14),
+                (24, 16),
+                (26, 18),
+                (37, 18),
+                (44, 25),
+            ]
+        },
+        "CHR2": {"TR2": [(10, 0), (30, 20)]},
+    }
+    knownValuesToCHR = (
+        ("TR1", 4, "CHR1", 7),
+        ("TR1", 14, "CHR1", "I24"),
+        ("TR1", 0, "CHR1", 3),
+        ("TR1", 24, "CHR1", 43),
+        ("TR2", 0, "CHR2", 10),
+        ("TR1", 13, "CHR1", 23),
+        ("TR2", 10, "CHR2", 20),
+    )
+    knownValuesToTR = (
+        ("CHR1", 7, "TR1", 4),
+        ("CHR1", 12, "TR1", "D8"),
+        ("CHR2", 10, "TR2", 0),
+        ("CHR1", 23, "TR1", 13),
+        ("CHR2", 20, "TR2", 10),
+    )
+
+    def testCreateTranslateTbl(self):
+        """createTranslateTbl should give known result"""
+        result_TR2CHR = translator.createTranslateTbl(self.CIGARObj)
+        self.assertEqual(self.knownTranslateTbl_TR2CHR, result_TR2CHR)
+        result_CHR2TR = translator.createTranslateTbl(self.CIGARObj, mode="CHR2TR")
+        self.assertEqual(self.knownTranslateTbl_CHR2TR, result_CHR2TR)
+
+    def testToCHRKnownValues(self):
+        """toCHR should give known result with known input"""
+        translateTbl = translator.createTranslateTbl(self.CIGARObj)
+        for item in self.knownValuesToCHR:
+            TRName, TR, CHRName, CHR = item
+            result, _ = translator.toCHR(translateTbl, TRName, TR)
+            self.assertEqual([item], result)
+
+    def testToTRKnownValue(self):
+        """toTR should give known result with known input"""
+        translateTbl = translator.createTranslateTbl(self.CIGARObj, mode="CHR2TR")
+        for item in self.knownValuesToTR:
+            CHRName, CHR, TRName, TR = item
+            result, _ = translator.toTR(translateTbl, CHRName, CHR)
+            self.assertEqual([item], result)
+
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
